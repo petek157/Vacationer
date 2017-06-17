e6ed5106ffd085474801356fb2be1f0c486d8e2d
petek157 <peterkoruga@peters-imac.att.net>
2017-06-17 16:04:57 -0500

    Figured how to create a round button.

---
+++ Vacationer/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Vacationer
+//
+//  Created by Peter Koruga on 6/17/17.
+//  Copyright © 2017 P3 Media. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppColors {
+    let buttons = UIColor.blue
+    
+}
--- Vacationer/ViewController.swift
+++ Vacationer/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var expandIconsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Testing Commit")
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        let myColors = AppColors()
+        expandIconsButton.backgroundColor = myColors.buttons
+        expandIconsButton.layer.cornerRadius = self.expandIconsButton.frame.width/2
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
